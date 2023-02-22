2297d18cc65f9d582dcf81a7a8969e9740234193
RooseveltTech <rooseveltabandy@gmail.com>
2023-02-22 12:27:27 +0100

    delete-migrations -v 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "delete-migration",
+    name = "delete-migrations",
     version = "1.0.0",
     
     author = "Abandy Roosevelt",
